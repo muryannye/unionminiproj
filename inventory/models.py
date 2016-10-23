865238503078b9eeb19d7287b10ef733a993feae
Mary Ly <Mary@MaruWaterloo.local>
2016-10-23 18:00:57 -0400

    Added folder/computer models, templates directory, and views

--- inventory/models.py
+++ inventory/models.py
@@ -2,8 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-class Computers(models.Model):
+@python_2_unicode_compatible
+class Folder(models.Model):
+    folder_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.folder_name
+
+@python_2_unicode_compatible
+class Computer(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True)
     manufacturer =  models.CharField(max_length=250)
     comments = models.CharField(max_length=300)
     serial_number = models.CharField(max_length=100)
+    def __str__(self):
+        return self.serial_number
--- inventory/views.py
+++ inventory/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Folder
 
-# Create your views here.
+def index(request):
+    all_folders = Folder.objects.all()
+    template = loader.get_template('inventory/index.html')
+    context = {
+        'all_folders': all_folders,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, folder_id):
+    try:
+        folder = Folder.objects.get(pk=folder_id)
+    except Folder.DoesNotExist:
+        raise Http404("Folder does not exist")
+    return render(request, 'inventory/detail.html', {'folder': folder})
