27b66fb1c337892bd984490b81239ebb3f34d7ae
Mary Ly <Mary@jumping-jack-139.dynamic2.rpi.edu>
2016-10-23 03:32:02 -0400

    Added Computers class

--- inventory/models.py
+++ inventory/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Computers(models.Model):
+    manufacturer =  models.CharField(max_length=250)
+    comments = models.CharField(max_length=300)
+    serial_number = models.CharField(max_length=100)
